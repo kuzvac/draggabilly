a74f6b94f8f27c0565a953dedb3445176fa9c91f
Vasiliy Kuzmenko <kuzvasiliy@gmail.com>
2016-02-12 01:56:48 +0500

    Remove reset top/left styles from destroy

--- dist/draggabilly.pkgd.js
+++ dist/draggabilly.pkgd.js
@@ -1502,8 +1502,6 @@ proto.destroy = function() {
   this.disable();
   // reset styles
   this.element.style[ transformProperty ] = '';
-  this.element.style.left = '';
-  this.element.style.top = '';
   this.element.style.position = '';
   // unbind handles
   this.unbindHandles();
